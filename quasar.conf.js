e1791d17f93522da9dc85ea013ee7caa5a98ba7c
TheAkshitS <akshit.s@outlook.com>
2021-06-01 03:14:33 +0530

    fix: app name

--- quasar.conf.js
+++ quasar.conf.js
@@ -117,8 +117,8 @@ module.exports = function (/* ctx */) {
       workboxPluginMode: 'GenerateSW', // 'GenerateSW' or 'InjectManifest'
       workboxOptions: {}, // only for GenerateSW
       manifest: {
-        name: `Quasar App`,
-        short_name: `Quasar App`,
+        name: `Aspire App`,
+        short_name: `Aspire App`,
         description: `A Quasar Framework app`,
         display: 'standalone',
         orientation: 'portrait',
