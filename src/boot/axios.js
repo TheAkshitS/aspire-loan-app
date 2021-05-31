5210bf444c4bd5bf9b75332f70f515511e19d788
TheAkshitS <akshit.s@outlook.com>
2021-05-31 23:14:44 +0530

    feat: set default Content-Type to application/json

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -3,7 +3,10 @@ import axios from 'axios'
 
 Vue.prototype.$axios = axios
 
-const api = axios.create({ baseURL: 'http://localhost:3000' })
+const api = axios.create({
+    baseURL: 'http://localhost:3000',
+    "Content-Type": "application/json"
+})
 
 Vue.prototype.$api = api
 
