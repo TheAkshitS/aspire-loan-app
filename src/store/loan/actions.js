bdaa212d9a5b12c4cb3d7ad9bd7e7f5dc252bc9e
TheAkshitS <akshit.s@outlook.com>
2021-06-01 11:30:42 +0530

    feat: return response for all actions

--- src/store/loan/actions.js
+++ src/store/loan/actions.js
@@ -2,14 +2,19 @@ import { api } from 'boot/axios'
 
 export async function getAllLoans({ commit }) {
   const response = await api.get('/loans')
-
   commit('SET_ALL_LOANS', response.data)
+
+  return response
 }
 
 export async function createLoan({ commit }, payload) {
-  await api.post('/loans', payload)
+  const response = await api.post('/loans', payload)
+
+  return response
 }
 
 export async function updateLoan({ commit }, { id, payload }) {
-  await api.patch(`/loans/${id}`, payload)
+  const response = await api.patch(`/loans/${id}`, payload)
+
+  return response
 }
